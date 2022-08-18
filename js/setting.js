0d7bb8ada98094575ddf48227b4fe02230f5514b
rumoikk <110016658+rumoikk@users.noreply.github.com>
2022-08-18 10:45:34 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -3,12 +3,12 @@
  *
 */
 
-var SVGLabel = alsfe; // SVGイメージを使用するときは、true。用意できない場合はfalse。
+var SVGLabel = false; // SVGイメージを使用するときは、true。用意できない場合はfalse。
 
 var MaxDescription = 23; // ごみの最大種類、９を超えない場合は変更の必要はありません。
 
 var MaxMonth = 12;
 
-var WeekShift = false; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
+var WeekShift = true; // 休止期間なら週をずらすときは、true。金沢の仕様は、true。
 
-var SkipSuspend = false; // 休止期間を除去するときは、true。奈良の仕様は、true。
+var SkipSuspend = true; // 休止期間を除去するときは、true。奈良の仕様は、true。
